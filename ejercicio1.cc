daa46b8d37724fc14031f85289378c22e227e2da
jenifer2015 <31485128+jenifer2015@users.noreply.github.com>
2017-10-18 13:58:04 -0500

    Add files via upload

---
+++ ejercicio1.cc
@@ -0,0 +1,19 @@
+//Autor: Dorantes Garcia Jenifer
+//Creado el 17 de Octubre de 2017
+#include <iostream>
+using namespace std;
+int main (){
+
+ int N=0;
+ cout<<"Dame un nùmero entero"<<endl;
+ cin>>N;
+
+ int M=1;
+ int F=1;
+ while((M!=N)){
+  M = M+1
+  F=F*M;
+}
+ Count<<"Valor final"<<endl;
+ return0;
+}
